1abf44918b45721ec36e90a549a04b78ec34b6bb
Umama Masroor <umamafatima319@gmail.com>
2023-09-23 23:54:26 -0700

    complete

---
+++ assests/js/main.js
@@ -0,0 +1,24 @@
+/*=============== SERVICES MODAL ===============*/
+// Get the modal
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalClose = document.querySelectorAll(".services__modal-close");
+
+// When the user clicks on the button, open the modal
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((mb, i) => {
+  mb.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalClose.forEach((mc) => {
+  mc.addEventListener("click", () => {
+    modalViews.forEach((mv) => {
+      mv.classList.remove("active-modal");
+    });
+  });
+});
